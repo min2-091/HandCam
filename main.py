fe459df0d41bd4b7c497ed52aecca0520e4ef746
min2-091 <71590246+min2-091@users.noreply.github.com>
2021-11-20 10:17:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,75 @@
-import cv2
+import cv2, sys
 import time
 import datetime
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 video = cv2.VideoCapture(0)
-# 비디오 파일의 경우 '경로/파일명.확장자'
-
 prev_time = 1
+FPS = 144
+prev_time = 1 #카매라
 FPS = 144
 a = 0
 
 while True:
 
     ret, frame = video.read()
-    frame = cv2.flip(frame, 1)
-
     current_time = time.time() - prev_time
-
-    if (ret is True) and (current_time > 1. / FPS):
-
-        prev_time = time.time()
-
-        cv2.imshow('VideoCapture', frame)
-
-        now = datetime.datetime.now().strftime("_%H-%M-%S")
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
+    now = datetime.datetime.now().strftime("_%H-%M-%S")#현재 시간변수
+    cv2.imshow("images", frame)
+        if (ret is True) and (current_time > 1. / FPS):
+        if cv2.waitKey(1) & 0xFF == ord(" "):
             break
-        elif k == 26:
+        elif cv2.waitKey(1) & 0xFF == ord("s"):
             print("캡쳐")
             a = str(now)
             cv2.imwrite("/Users/jiminkim/Desktop/Capture/" + str(now) + ".png", frame)
-        elif k == 24:
-            print("최근 파일 색상 검출")
-            img_color = cv2.imread('/Users/jiminkim/Desktop/Capture/' + a + '.png')
-            print('shape: ', img_color.shape)
-            height, width = img_color.shape[:2]
-            img_hsv = cv2.cvtColor(img_color, cv2.COLOR_BGR2HSV)
-
-            lower_blue = (120-10, 30, 30)#R:140 G:120 B:100
-            upper_blue = (120+10, 255, 255)#R:220 G:200 B:170
-            img_mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-
-            img_result = cv2.bitwise_and(img_color, img_color, mask=img_mask)
-            cv2.imshow('img_origin', img_color)
-            cv2.imshow('img_mask', img_mask)
-            cv2.imshow('img_color', img_result)
-            cv2.waitKey()
-            cv2.destroyAllWindows()
-        elif k == 3:
-            print("최근 파일 모서리 검출")
-            img = cv2.imread('/Users/jiminkim/Desktop/Capture/' + a + '.png', cv2.IMREAD_COLOR)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            #모서리 검출
-            corner = cv2.cornerHarris(gray, 2, 3, 0.16)
-            #10% 지표구하기
-            coord = np.where(corner > 0.05 * corner.max())
-            coord = np.stack((coord[1], coord[0]), axis=-1)
-
-            for x, y in coord:
-                cv2.circle(img, (x, y), 5, (0, 0, 255), 1, cv2.LINE_AA)
-
-            corner_norm = cv2.normalize(corner, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-
-            corner_norm = cv2.cvtColor(corner_norm, cv2.COLOR_GRAY2BGR)
-            merged = np.hstack((corner_norm, img))
-            cv2.imshow('Harris Corner', merged)
-            cv2.waitKey()
+        elif cv2.waitKey(1) & 0xFF == ord("z"):
+            print("이미지 테두리 분석")
+            cv2.waitKey(0)
             cv2.destroyAllWindows()
 
+            img = cv2.imread("/Users/jiminkim/Desktop/Capture/" + a + ".png")
+            img_copy = img.copy()
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(img, contours, -1, (0, 255, 255), 2)
+
+            c0 = contours[0]
+            M = cv2.moments(c0)
+            print(M.items())
+
+            plt.title('contour')
+            plt.imshow(img)
+            plt.axis('off')
 
 
-#img_color = cv2.imread(str(now) + '.png')  # 이미지 파일을 컬러로 불러옴
-#print('shape: ', img_color.shape)
-# 색 추출 -> 형태인식 -> 꼭직점 인식 -> 비율 비교 -> 출력
+            img_copy = img.copy()
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            cv2.circle(img_copy, (cx, cy), 15, (255, 0, 0), -1)
+
+            plt.imshow(img_copy)
+            plt.title("Contour Center")
+            plt.axis("off")
+            plt.show()
+
+
+            leftmost = tuple(c0[c0[:, :, 0].argmin()][0])
+            rightmost = tuple(c0[c0[:, :, 0].argmax()][0])
+            topmost = tuple(c0[c0[:, :, 1].argmin()][0])
+            bottommost = tuple(c0[c0[:, :, 1].argmax()][0])
+
+            cv2.circle(img_copy, (leftmost[0], leftmost[1]), 10, (0, 0, 255), -1)
+            cv2.circle(img_copy, (rightmost[0], rightmost[1]), 10, (0, 0, 255), -1)
+            cv2.circle(img_copy, (bottommost[0], bottommost[1]), 10, (0, 0, 255), -1)
+            cv2.circle(img_copy, (topmost[0], topmost[1]), 10, (0, 0, 255), -1)
+
+            plt.imshow(img_copy)
+            plt.title("Extream Point")
+            plt.axis("off")
+            plt.show()
+
